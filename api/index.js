3be16ef54902e207096e215445a92c2b2c60e684
ANKITKUMAR <ankitkumarjl361@gmail.com>
2024-02-29 02:24:11 +0530

    pre installation

--- api/index.js
+++ api/index.js
@@ -9,6 +9,7 @@ import hotelRouter from "./routes/myhotel.routes.js"
 import hotelsRouter from "./routes/hotels.routes.js"
 import mybookingRouter from "./routes/mybooking.routes.js"
 import cookieParser from 'cookie-parser';
+import path from "path"
 import { v2 as cloudinary } from 'cloudinary';
 const corOption={
     credentials:true,origin:['http://localhost:5173']
@@ -20,6 +21,7 @@ cloudinary.config({
     api_key:process.env.CLOUDINARY_API_KEY,
     api_secret:process.env.CLOUDINARY_SECRET_KEY
 })
+const __dirname=path.resolve()
 app.use(cookieParser())
 app.use(cors(corOption))
 ConnectDB()
@@ -29,6 +31,10 @@ app.use("/api/users",userRouter)
 app.use("/api/my-hotels",hotelRouter) 
 app.use("/api/hotels",hotelsRouter)
 app.use('/api/mybooking',mybookingRouter)
+app.use(express.static(path.join(__dirname,'/client/dist')))
+app.get('*',(req,res)=>{
+    res.sendFile(path.join(__dirname,'client','dist','index.html'))
+})
 const server=app.listen(8000,()=>{
     console.log(`Server Started at port ${server.address().port} `)
 })
